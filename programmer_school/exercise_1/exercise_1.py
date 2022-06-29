67ca5a8e58fef8442c700595d3c0d53bc18e6e3e
Alexander <321gizatulin@gmail.com>
2022-06-29 17:29:33 +1100

    exercise 1 done
    yaho!

--- programmer_school/exercise_1/exercise_1.py
+++ programmer_school/exercise_1/exercise_1.py
@@ -1,8 +1,14 @@
-def summary(input_file):
+def summary(input_file, output_file):
     file = open(input_file,'r')
+    result = open(output_file, 'w')
     for line in file:
         line = line.split(' ')
-        print(line[0])
+        summ = int(line[0]) + int(line[1])
+        if summ > 10**9:
+            result.write('INVALID NUMBER' + '\n')
+        else:
+            result.write(str(summ) + '\n')
 
 
-summary('INPUT.txt')
+summary('INPUT.txt', 'OUTPUT.txt')
+
