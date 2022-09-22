396857e2a531907e9b9bc415f274b94fa5ac5c3b
Alexander <321gizatulin@gmail.com>
2022-09-22 16:01:39 +1100

    Create DREAM.py
    ~~~~~~~~~~~~!~~~~~~~~~~~~~

---
+++ programmer_school/DREAM/DREAM.py
@@ -0,0 +1,9 @@
+def dream(message):
+    wish = message
+    my_will = 'I can make a wish, and it is comes true'
+    return True
+
+
+if dream('I wish to all the people of the world, to make this life beautiful adventure. Try to like your neighbor. '
+         'Do your best. Make some friends. Make love. Create. Imagine.'):
+    print('It is DONE')
